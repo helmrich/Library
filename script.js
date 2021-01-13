2d9009a7f87b3e6443540b1ead6562ef999399e1
Tobias Helmrich <tobias.g.helmrich@gmail.com>
2021-01-13 22:40:30 +0100

    Create modal for adding new books and add functionality to open and close it, make UI improvements

--- script.js
+++ script.js
@@ -47,8 +47,15 @@ myLibrary.map(book => {
 
 const addBookArticle = document.createElement("article");
 addBookArticle.classList.add("book-card", "add-book-card");
-const plusIcon = document.createElement("img");
-plusIcon.setAttribute("src", "plus-circle.svg");
-plusIcon.classList.add("plus-icon");
-addBookArticle.appendChild(plusIcon);
+addBookArticle.innerHTML += '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="0.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-circle plus-icon"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg>';
 document.getElementById("bookList").appendChild(addBookArticle);
+
+// Event Listeners
+
+document.querySelector(".add-book-card").addEventListener("click", (e) => {
+    document.querySelector("#bookInputModalContainer").classList.toggle("hidden");
+});
+
+document.querySelector("#bookInputModalTop .close-icon").addEventListener("click", (e) => {
+    document.querySelector("#bookInputModalContainer").classList.toggle("hidden");
+});
